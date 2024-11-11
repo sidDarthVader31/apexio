26a0e64f0ad710c4edf7eb63ee0a6466761ad4df
Siddharth Bisht <siddharthbisht31@gmail.com>
2024-11-12 01:29:10 +0530

    add a structured payload to ingest log rest api

--- log_ingestion_service/source_web/controller.go
+++ log_ingestion_service/source_web/controller.go
@@ -8,13 +8,31 @@ import (
 
 type LogInfo struct {
 	Id        uint                    `json:"id"`
-	Metadata  map[string]string       `json:"metadata"` 
+	Metadata  metadata                `json:"metadata"` 
   Timestamp uint64                  `json:"timestamp"`
   Loglevel  string                  `json:"logLevel"`
-  message   string                  `json:"message"`
-  Source    map[string]string       `json:"source"`
+  Message   string                  `json:"message"`
+  Source    source                  `json:"source"`
 }
 
+
+type metadata struct {
+  request_id string                 `json:"requestId"` 
+  client_ip  string                 `json:"clientIp"` 
+  user_agent string                 `json:"userAgent"` 
+  request_method string             `json:"requestMethod"` 
+  request_path string               `json:"requestPath"` 
+  response_status string            `json:"responseStatus"` 
+  response_duration string          `json:"responseDuration"` 
+  extra map[string] string          `json:"extra"` 
+}
+
+type source struct{
+  host string                       `json:"host"` 
+  service string                    `json:"service"` 
+  environment string                `json:"environment"` 
+  extra map[string] string          `json:"extra"` 
+}
 func ingestData(c *gin.Context) {
   var logInfo LogInfo
   if err := c.BindJSON(&logInfo); err != nil {
