ea5c0378d428a175c3bc43d9530db22c7ecc7f73
Siddharth Bisht <siddharthbisht31@gmail.com>
2024-11-15 15:59:23 +0530

    add grpc service for log ingestion

---
+++ log_ingestion_service/source_grpc/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+
+
+type logServer struct {
+  UnimplementedLoggingServiceServer
+}
+
+ func (s *logServer) IngestLog( ctx context.Context, in *LogRequest) (*LogResponse, error) {
+  fmt.Println("in:", in)
+  value, err := json.Marshal(in)
+  if err != nil {
+    return nil, err
+  }
+  ingestToKafka(value, logTopic);
+  res := LogResponse{
+    Message: "log ingested successfully",
+    Success: true,
+  }
+  return &res, nil;
+}
+
+func main(){
+  flag.Parse()
+  connectKafka();
+  lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+  if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+  s := grpc.NewServer()
+  RegisterLoggingServiceServer(s, &logServer{})
+  if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+
+
+
+
+
+
+
+
+
