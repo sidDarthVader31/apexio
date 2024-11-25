26d104b7e1f26eb554e1dc48a30f101bce086aa4
Siddharth Bisht <siddharthbisht31@gmail.com>
2024-11-25 11:30:01 +0530

    add kafka data stream service and batch processing to grpc ingestor

---
+++ log_ingestion_service/source_grpc/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+
+
+
+type IConfig struct{
+  KAFKA_HOST string
+  KAFKA_USER string
+  KAFKA_PASSWORD string
+  PORT string
+  MESSAGE_BROKER string
+}
+
+var Config IConfig
+func InitEnv() error{
+  viper.SetConfigFile(".env")
+  err := viper.ReadInConfig()
+  if err != nil{
+    fmt.Println("Error while reading env file:", err)
+    return err
+  }
+  initEnvVariables()
+  return nil
+}
+
+func initEnvVariables(){
+  Config.KAFKA_HOST = viper.GetString("KAFKA_HOST")
+  Config.MESSAGE_BROKER = viper.GetString("MESSAGE_BROKER")
+  Config.KAFKA_USER = viper.GetString("KAFKA_USER")
+  Config.KAFKA_PASSWORD = viper.GetString("KAFKA_PASSWORD")
+  Config.PORT = viper.GetString("PORT")
+}
---
+++ log_ingestion_service/source_grpc/services/dataservice/dataStreamService.go
@@ -0,0 +1,67 @@
+package dataservice
+
+import (
+	"context"
+	"errors"
+	"sync"
+)
+
+// ------------------------------------   data stream
+// service interface and utils
+
+type IDataStreamService interface{
+  Connect(context context.Context, config map[string]string) error
+  ProduceMessage(context context.Context, message []byte, topicName string) (bool, error)
+  Close()
+}
+
+
+type DataStreamService struct{
+  service IDataStreamService
+}
+
+
+
+func (d *DataStreamService) Connect(context context.Context, config map[string]string) error{
+  return d.service.Connect(context, config)
+}
+
+func (d *DataStreamService) ProduceMessage(context context.Context, message []byte, topicName string) (bool, error){
+  return d.service.ProduceMessage(context, message, topicName)
+}
+
+func (d *DataStreamService) Close(){
+ d.service.Close()
+}
+
+
+
+// --------------------------------------- data stream
+// service factory
+
+var (
+    StreamService *DataStreamService
+    once          sync.Once // Ensure initialization is thread-safe
+)
+
+
+func GetDataStreamService(messaseService string, config map[string]string) (*DataStreamService, error){
+	var service IDataStreamService
+   once.Do(func() {
+        StreamService = &DataStreamService{}
+    })
+	var err error
+
+
+  switch(messaseService){
+  case "KAFKA":
+    service, err = getNewkafkaService(config)
+  if err!=nil{
+      return nil, err
+    }
+    StreamService.service = service
+    return StreamService, nil
+  default:
+    return nil, errors.New("Invalid message service option")
+  }
+}
