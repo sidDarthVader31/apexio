f9b3209d2bdc5d0c81dcdfd1e7a9e30b3bc13b30
Siddharth Bisht <siddharthbisht31@gmail.com>
2024-12-07 14:43:05 +0530

    fix env for grpc service and kafka message producing logic

--- log_ingestion_service/source_grpc/main.go
+++ log_ingestion_service/source_grpc/main.go
@@ -14,10 +14,6 @@ import (
 	"google.golang.org/grpc"
 )
 
-var (
-	port = flag.Int("port",  config.Config.PORT, "The server port")
-) 
-
 var DataStreamService *dataservice.DataStreamService;
 var err error
 
@@ -32,7 +28,14 @@ type logServer struct {
   if err != nil {
     return nil, err
   }
-  DataStreamService.ProduceMessage(context.Background(), value, constants.LogTopic)
+   _, err = DataStreamService.ProduceMessage(context.Background(), value, constants.LogTopic)
+  if err != nil {
+    res := LogResponse{
+      Message: "error ingesting log",
+      Success: false,
+    }
+    return &res, nil
+  }
   res := LogResponse{
     Message: "log ingested successfully",
     Success: true,
@@ -42,20 +45,27 @@ type logServer struct {
 
 func main(){
   flag.Parse()
-  DataStreamService, err = dataservice.GetDataStreamService(config.Config.MESSAGE_BROKER, map[string]string{"baseUrl": config.Config.KAFKA_HOST})
+  config.InitEnv()
+  DataStreamService, err = dataservice.GetDataStreamService(config.Config.MESSAGE_BROKER)
   if err!= nil{
     log.Fatalf("Error connecting with kafka : %v", err)
     os.Exit(1)
   }
-  DataStreamService.Connect(context.Background(), map[string]string{"baseUrl":config.Config.KAFKA_HOST})
-  lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+   connectErr := DataStreamService.Connect(context.Background())
+  if connectErr != nil{
+    fmt.Println("error connecting to data service:", connectErr)
+  }
+
+  lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *&config.Config.PORT))
   if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
   s := grpc.NewServer()
-  RegisterLoggingServiceServer(s, &logServer{})
   if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+
+  RegisterLoggingServiceServer(s, &logServer{})
+
 }
 
