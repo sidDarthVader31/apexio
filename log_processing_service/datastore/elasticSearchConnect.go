1c22b543841cff35c5d0ae80b844d078510a8813
Siddharth Bisht <siddharthbisht31@gmail.com>
2024-12-08 15:20:59 +0530

    add env support to elatcisearch

--- log_processing_service/datastore/elasticSearchConnect.go
+++ log_processing_service/datastore/elasticSearchConnect.go
@@ -3,12 +3,12 @@ package datastore
 import (
 	"fmt"
 	"log-processor/config"
-
 	"github.com/elastic/go-elasticsearch/v8"
 )
 var Es *elasticsearch.Client
 var err error
 func ConnectToElastic() (*elasticsearch.Client, error){
+  fmt.Println("elastic host:", config.Config.ELASTIC_HOST)
   cfg := elasticsearch.Config{
     Addresses: []string{
       config.Config.ELASTIC_HOST,
@@ -17,10 +17,11 @@ func ConnectToElastic() (*elasticsearch.Client, error){
     Password: config.Config.ELASTIC_PASSWORD,
   }
   Es, err = elasticsearch.NewClient(cfg);
+
   if err != nil {
     fmt.Println("connection to elastic search failed:", err)
   }else{
-    fmt.Println("successfully connected to elastic search :", err)
+    fmt.Println("successfully connected to elastic search :")
   }
   fmt.Println("elastic client:", &Es)
   return Es, err
